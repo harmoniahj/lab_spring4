dc5af9bc0948c69000c1b7e0f6affdb25f9f1283
harmoniahj <harmoniahj@naver.com>
2021-05-31 16:26:13 +0900

    05월 31알 BoardController, BoardLogic 추가

--- spring4-1_1/src/web/mvc/Board41Controller.java
+++ spring4-1_1/src/web/mvc/Board41Controller.java
@@ -1,5 +1,14 @@
 package web.mvc;
 
-public class Board41Controller {
-	
+import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
+
+public class Board41Controller extends MultiActionController {
+ // <bean id="board-controller">
+ //	 <property name="boardLogic"> -> setter메소드 명
+	private Board41Logic boardLogic = null;
+
+ // setter 메소드를 통해 게으른 객체 주입
+	public void setBoardLogic(Board41Logic boardLogic) {
+		this.boardLogic = boardLogic;
+	}
 }
